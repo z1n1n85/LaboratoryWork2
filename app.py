e92f00cb698cd83bf52114e99ed979224df6e6a6
Кристина <kristinakazakova596@gmail.com>
2024-12-01 01:37:55 +0300

    Добавила структуру навигации(шапку)

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def start():
-    return 'Hello,world'
+def mainPage():
+    return render_template('mainPage.html')
 
+
+@app.route("/addArr")
+def addArr():
+    return render_template('addArray.html')
+
+
+@app.route("/editArr")
+def editArr():
+    return render_template('editArray.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
