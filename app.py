9b0644b3d194543f2244f9c28321d2529f3b675b
Кристина <kristinakazakova596@gmail.com>
2024-12-01 15:28:32 +0300

    Изменила структуру навигации,стиль оформления, добавила действия с массивами пока без бд

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+arrays = []
 
-@app.route("/")
-def mainPage():
-    return render_template('mainPage.html')
+@app.route('/')
+def main_page():
+    return render_template('mainPage.html', arrays=arrays)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add_array():
+    if request.method == 'POST': #проверка метода
+        array_data = request.form['array'] #получаем массив
 
-@app.route("/addArr")
-def addArr():
+        # Преобразуем строку в список чисел и добавляем в общий список
+        array_numbers = [int(num) for num in array_data.split(',')]
+        arrays.append(array_numbers)
+
+        return redirect(url_for('main_page')) #перенаправляем на главную
     return render_template('addArray.html')
 
+@app.route('/edit/<int:index>', methods=['GET', 'POST'])
+def edit_array(index):
+    if request.method == 'POST':
+        array_data = request.form['array']
+        array_numbers = [int(num) for num in array_data.split(',')]
 
-@app.route("/editArr")
-def editArr():
-    return render_template('editArray.html')
+        arrays[index] = array_numbers  # Обновляем массив по индексу
+        return redirect(url_for('main_page'))
+
+    current_array = arrays[index]
+    return render_template('editArray.html', current_array=current_array, index=index)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
