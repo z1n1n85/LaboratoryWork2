1d0124e1ac9f97adb8891378e59ff9e46253386f
Кристина <kristinakazakova596@gmail.com>
2024-12-08 01:36:59 +0300

    прописала сортировку массивов, прописала кучу стилей

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
-from db import Database
 import json
+from db import Database
+from sorting import sort_bitonic
 
-app = Flask(__name__)
+app = Flask(__name__)  # Исправлено на __name__
 app.secret_key = 'keyyyyyyyyyyyyyyyyyyy'
 
 # Создаем экземпляр базы данных
@@ -11,50 +12,67 @@ db = Database()
 @app.route('/')
 def main_page():
     arrays = db.get_all_arrays()  # Получаем все массивы из базы данных
-
     return render_template('mainPage.html', arrays=arrays)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_array():
     if request.method == 'POST':
         array_data = request.form['array']
-
-        # Преобразуем строку в массив целых чисел
         try:
             number_array = list(map(int, array_data.split()))
-            db.add_array(number_array)  # Добавляем массив в базу данных
-
-            flash('Массив успешно добавлен!', 'success')  # Уведомление об успешном добавлении
+            db.add_array(number_array)  # Просто добавляем массив в базу данных
+            flash('Массив успешно добавлен!', 'success')
         except ValueError:
             flash('Ошибка: убедитесь, что вы ввели только числа, разделенные пробелами.', 'error')
-
-        return redirect(url_for('main_page'))  # Перенаправляем на главную страницу
-
+        return redirect(url_for('main_page'))
     return render_template('addArray.html')
 
 @app.route('/edit/<int:index>', methods=['GET', 'POST'])
 def edit_array(index):
-    current_array = db.get_array_by_id(index)  #Получаем массив по индексу
+    current_array = db.get_array_by_id(index)
 
     if not current_array:
-        flash('Массив не найден!', 'error')  # Уведомление об ошибке
+        flash('Массив не найден!', 'error')
         return redirect(url_for('main_page'))
 
     if request.method == 'POST':
+        if 'delete' in request.form:
+            db.delete_array(index)  # Удаляем массив из базы данных
+            flash('Массив успешно удален!', 'success')
+            return redirect(url_for('main_page'))  # Перенаправляем на главную страницу
+
+        if 'sort' in request.form:
+            sorted_array = sort_bitonic(current_array.array_data)  # Используем битонную сортировку
+            db.update_array(index, sorted_array)  # Сохраняем отсортированный массив в базе данных
+            flash('Массив успешно отсортирован!', 'success')
+            return redirect(url_for('edit_array', index=index))  # Перенаправляем на ту же страницу
+
         new_data = request.form['array']
         try:
             number_array = list(map(int, new_data.split()))
             db.update_array(index, number_array)
-
-            flash('Массив успешно обновлен!', 'success')  # Уведомление об успешном обновлении
+            flash('Массив успешно обновлен!', 'success')
         except ValueError:
             flash('Ошибка: убедитесь, что вы ввели только числа, разделенные пробелами.', 'error')
 
         return redirect(url_for('main_page'))
 
-    # Преобразуем массив в строку для отображения в окне
     array_string = ' '.join(map(str, current_array.array_data))
     return render_template('editArray.html', current_array=array_string)
 
-if __name__ == '__main__':  # Исправлено: __name__ вместо name
+@app.route('/sort/<int:index>', methods=['POST'])
+def sort_array(index):
+    current_array = db.get_array_by_id(index)
+
+    if not current_array:
+        flash('Массив не найден!', 'error')
+        return redirect(url_for('main_page'))
+
+    sorted_array = sort_bitonic(current_array.array_data)  # Используем битонную сортировку
+
+    db.update_array(index, sorted_array)  # Сохраняем отсортированный массив в базе данных
+    flash('Массив успешно отсортирован!', 'success')
+    return redirect(url_for('main_page'))
+
+if __name__ == '__main__':  # Исправлено на __name__
     app.run(debug=True)
--- db.py
+++ db.py
@@ -14,7 +14,6 @@ class Database:
     def get_connection(self):
         conn = sqlite3.connect(self.database)
         conn.row_factory = sqlite3.Row
-
         return conn
 
     def init_db(self):
@@ -30,34 +29,32 @@ class Database:
 
     def add_array(self, array_data):
         json_data = json.dumps(array_data)  # Сохраняем массив как JSON
-
         conn = self.get_connection()
         conn.execute('INSERT INTO arrays (array_data) VALUES (?)', (json_data,))
-
         conn.commit()
         conn.close()
 
     def get_all_arrays(self):
         conn = self.get_connection()
-
         arrays = conn.execute('SELECT * FROM arrays').fetchall()
         conn.close()
-
         return [Array(array['id'], json.loads(array['array_data'])) for array in arrays]  # Создание объектов Array
 
     def get_array_by_id(self, array_id):
         conn = self.get_connection()
-
         array = conn.execute('SELECT * FROM arrays WHERE id = ?', (array_id,)).fetchone()
         conn.close()
-
         return Array(array['id'], json.loads(array['array_data'])) if array else None  # Возвращаем объект Array
 
     def update_array(self, array_id, array_data):
         json_data = json.dumps(array_data)  # Сохраняем массив как JSON
-
         conn = self.get_connection()
         conn.execute('UPDATE arrays SET array_data = ? WHERE id = ?', (json_data, array_id))
+        conn.commit()
+        conn.close()
 
+    def delete_array(self, array_id):
+        conn = self.get_connection()
+        conn.execute('DELETE FROM arrays WHERE id = ?', (array_id,))
         conn.commit()
         conn.close()
